c9ab59ff7254782bd671ec2ff8ab9c62bb81cfec
chizitga <vipmath171@gmail.com>
2021-12-05 15:52:12 +0700

    Initial deploy

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, request, jsonify, Response
+from model_process.regression_model_2 import predict
+from flask_cors import CORS, cross_origin
+import pandas as pd
+from firebase.firebase import db;
+from firebase_admin import firestore
+from datetime import datetime
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    
+@app.route("/realtime-data", methods=['POST', 'GET'])
+def get_realtime_data():
+    return "GET DATA REALTIME"
+    
+@app.route('/fetch-data', methods=['POST', 'GET'])
+def fetch_data():
+    df = pd.read_csv("data/VN-covid19.csv", on_bad_lines='skip')
+        
+    dates = df['ObservationDate'].tolist()
+    confirmed = df['Confirmed'].tolist()
+    
+    for i in range(len(dates)):
+        datetime_object = datetime.strptime(dates[i] + "-2021",'%d-%b-%Y').date()
+        province_id = 0
+        doc_ref = db.collection(u'data').document(datetime_object.__str__() + "_" + province_id.__str__())
+        if doc_ref.get().exists:
+            continue
+        else:
+            doc_ref.set({
+                u'province_id' : province_id,
+                u'date': datetime_object.__str__(),
+                u'confirmed': confirmed[i]
+            })
+        
+        
+    return jsonify({"code": 1})
+
+def extract_firebase_item(item):
+    return item.to_dict()
+
+def sort_key(item):
+    return item['date']
+
+@app.route('/get-data', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        province_id = request.form['province_id']
+        next_days_num = request.form['next_days_num']
+        if not province_id: 
+            return jsonify({"code": 1})
+        else:
+            print(province_id)
+            db_ref = db.collection(u'data').where(u'province_id', u'==', int(province_id)).order_by(u'date', direction=firestore.Query.DESCENDING)
+            data_items = list(map(extract_firebase_item,db_ref.get()))
+            data_items.sort(key=sort_key)
+            df = pd.DataFrame()
+            date_data = []
+            confirm_data = []
+            print(data_items)
+            for item in data_items:
+                extract_item = item
+
+                date_data.append(extract_item['date'])
+                confirm_data.append(extract_item['confirmed'])
+            df['ObservationDate'] = date_data
+            df["Confirmed"] = confirm_data
+            data = predict(df, int(next_days_num))
+            return jsonify({
+                "code": 1,
+                "data": data
+            })
+            
+    return jsonify({"code": 1})
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
---
+++ model_process/regression_model_2.py
@@ -0,0 +1,64 @@
+import numpy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+import ntpath
+import os
+from sklearn.pipeline import make_pipeline
+
+def array_to_single_index(arr):
+    return arr[0]
+
+def predict(df, x):
+    days_since_lst = []
+    length = len(df)
+    for i in range(length):
+        days_since_lst.append(i)
+
+    date = np.array(df['ObservationDate'])
+    X_train = np.array(df["Confirmed"])  # .reshape(-1,1)
+    # X is the world cases array
+    y_train = np.array(days_since_lst).reshape(-1, 1)
+
+    for i in range(length, length + x):
+        days_since_lst.append(i)
+    y = np.array(days_since_lst).reshape(-1, 1)
+
+    days_since_lst = []
+    for i in range(length, length + x):
+        days_since_lst.append(i)
+
+    y_test = np.array(days_since_lst).reshape(-1, 1)
+
+    poly = PolynomialFeatures(degree=6)
+    poly_y_train = poly.fit_transform(y_train)
+    poly_y_test = poly.fit_transform(y_test)
+    poly_y = poly.fit_transform(y)
+
+    linear_model = make_pipeline(StandardScaler(with_mean=False), LinearRegression())
+    linear_model.fit(poly_y_train, X_train)
+    test_linear_pred = linear_model.predict(poly_y_test)
+    linear_pred = linear_model.predict(poly_y)
+
+    file_name = "Predict_for_Test"
+    output_file_path =  "data/" + file_name
+
+    df_output = pd.DataFrame()
+    df_output['predict_confirmed_case'] = linear_pred
+    for i in range(x):
+        X_train = numpy.append(X_train, 0)
+        date = numpy.append(date, 'Null')
+    df_output['confirmed_case'] = X_train
+    df_output['observation_date'] = date
+    df_output.to_csv(output_file_path)
+    print(df_output)
+    
+    return {
+        "linear_pred_y" : list(map(array_to_single_index, y.tolist())),
+        "linear_pred_x": linear_pred.tolist(),
+        "train_data_y": list(map(array_to_single_index, y_train.tolist())),
+        "train_data_x" : X_train.tolist()
+    }
